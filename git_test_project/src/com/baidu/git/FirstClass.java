f9b601115a9ca6e51db94f6b9c431000283d3654
root <root@qq.com>
2018-11-27 16:10:50 +0800

    把本地工程添加到git

---
+++ git_test_project/src/com/baidu/git/FirstClass.java
@@ -0,0 +1,10 @@
+package com.baidu.git;
+
+public class FirstClass {
+    public static void main(String args[]){
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
